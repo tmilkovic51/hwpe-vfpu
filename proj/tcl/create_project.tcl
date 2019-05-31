6e819f9f78da42fd3831a07bda6b1bcac358774f
Luka Macan <luka.macan@fer.hr>
2019-05-31 12:19:40 +0200

    Synthesis additions

--- proj/tcl/create_project.tcl
+++ proj/tcl/create_project.tcl
@@ -1,5 +1,8 @@
 create_project simulation .
 add_files -scan_for_includes -fileset sources_1 { .. }
 update_compile_order -fileset sources_1
+set_property board_part em.avnet.com:zed:part0:1.4 [current_project]
 set_property top hwpe_test_tb [get_filesets sim_1]
+set_property top hwpe_test_tb [get_filesets sources_1]
 set_property top_lib xil_defaultlib [get_filesets sim_1]
+update_compile_order -fileset sources_1
